c155911e1b5b61a6ec84268f7fb933c72d36c3cc
Alrik He <alrik_he@hotmail.com>
2024-09-09 22:08:57 +0200

    updated

--- app/src/main/java/se/gritacademy/navigatortest/MainActivity.kt
+++ app/src/main/java/se/gritacademy/navigatortest/MainActivity.kt
@@ -1,20 +1,28 @@
 package se.gritacademy.navigatortest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
       //  supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, BlankFragment()).addToBackStack("frag1").commit()
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+         navController = navHostFragment.navController
+
+        findViewById<Button>(R.id.button2).setOnClickListener({
+            navController.navigate(R.id.mainActivity2);
+        })
 
         findViewById<Button>(R.id.button).setOnClickListener {
             Log.i("alrik", "onCreate: "+navController.currentDestination!!.toString())
@@ -24,7 +32,6 @@ class MainActivity : AppCompatActivity() {
                 navController.navigate(R.id.action_blankFragment2_to_blankFragment)
             else
                 navController.navigate(R.id.action_blankFragment_to_blankFragment2)
-
 
 
         }
