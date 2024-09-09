f032f7216d4b295bcf458f4421c23e02e663d82f
Alrik He <alrik_he@hotmail.com>
2024-09-09 11:35:48 +0200

    updated

--- app/src/main/java/se/gritacademy/navigatortest/MainActivity.kt
+++ app/src/main/java/se/gritacademy/navigatortest/MainActivity.kt
@@ -2,6 +2,7 @@ package se.gritacademy.navigatortest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.navigation.fragment.NavHostFragment
 
@@ -14,9 +15,18 @@ class MainActivity : AppCompatActivity() {
       //  supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, BlankFragment()).addToBackStack("frag1").commit()
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        
+
         findViewById<Button>(R.id.button).setOnClickListener {
-            v->   navController.navigate(R.id.action_blankFragment_to_blankFragment2)
+            Log.i("alrik", "onCreate: "+navController.currentDestination!!.toString())
+
+            Log.i("alrik", "onCreate: "+navController.currentDestination!!.toString()+navController.currentDestination!!.id)
+            if(navController.currentDestination!!.id == R.id.blankFragment2)
+                navController.navigate(R.id.action_blankFragment2_to_blankFragment)
+            else
+                navController.navigate(R.id.action_blankFragment_to_blankFragment2)
+
+
+
         }
 
         //view.findNavController().navigate(R.id.viewTransactionsAction)
